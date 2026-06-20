d2da0315da76d882f206d64ab31f9bac917274bd
Rakha Avilla <azmyervawanrakhaavilla@gmail.com>
2026-06-20 21:21:23 +0700

    Next Tomr

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminPassword = await bcrypt.hash("admin123", 10);
+
+  await prisma.user.upsert({
+    where: {
+      email: "admin@avidev.studio",
+    },
+    update: {},
+    create: {
+      name: "Admin AviDev",
+      email: "admin@avidev.studio",
+      password: adminPassword,
+    },
+  });
+
+  console.log("Seed berhasil");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
